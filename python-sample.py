5c48aad5972abe51625719da47afd9cd459499b1
ryuya-matsunawa <ruymtnw@gmail.com>
2022-09-21 11:00:55 +0900

    feat: trophy workflow

---
+++ python-sample.py
@@ -0,0 +1,13 @@
+import requests
+
+for column in (4, 8):
+    for theme in ('onedark', 'flat'):
+        if column == 4:
+            r = requests.get(f"https://github-profile-trophy.vercel.app/?username=ryuya-matsunawa&theme={theme}&column={column}&no-frame=true&margin-w=15&margin-h=15")
+        else:
+            r = requests.get(f"https://github-profile-trophy.vercel.app/?username=ryuya-matsunawa&theme={theme}&column={column}&no-frame=true&margin-w=15")
+        
+        print(column, theme)
+        file = open(f"./profile-trophy-output/trophy-{theme}-column-{column}.svg", mode="w")
+        file.write(r.text)
+        file.close()
